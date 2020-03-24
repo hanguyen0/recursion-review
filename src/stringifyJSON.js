98201c2609a223c1896b8bb8c616faebe19e634e
risingbirdsong <heregoingthere@outlook.com>
2020-03-23 23:27:05 -0700

    hmm, with this debugging step, it sure looks like the outer array should be stringify, but for some reason the wrapping array is being ignored, getting tired, return later, get help on this issue

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,6 +9,11 @@ var stringifyJSON = function (obj) {
   // your code goes here
   //innerfunc
   //obj === string
+  debugging = false;
+  if (arguments[1] === "debug") {
+    debugging = true;
+    console.log("debuggingg!");
+  }
   if (typeof obj === 'string') {
     return `"${obj}"`
   }
@@ -19,9 +24,6 @@ var stringifyJSON = function (obj) {
     return "" + obj
   }
   //obj === undefined //obj === function
-  if (typeof obj === undefined || typeof obj === "function") {
-    return undefined;
-  }
 
 
   if (obj === null) {
@@ -37,6 +39,12 @@ var stringifyJSON = function (obj) {
   //obj === array
   if (Array.isArray(obj)) {
     // return `[${stringifyJSON(obj)}]`
+    // console.log("this one hitting? arrayyyy", obj)
+    if (debugging === true) {
+      console.log("in array", obj);
+      let mappedtest = `[${obj.map((val) => stringifyJSON(val))}]`
+      console.log("mappedTest", mappedtest);
+    }
     return `[${obj.map((val) => stringifyJSON(val))}]`
     //iterate
   }
@@ -44,14 +52,15 @@ var stringifyJSON = function (obj) {
   // //obj === obj
   if (typeof obj === "object") {
     // let len = Object.entries(obj).length;
-    console.log("what is failing", obj);
 
     if (Object.entries(obj).length === 0) {
       return "{}";
     }
     // console.log("entries", );
     // console.log("hitting");
-
+    if (debugging === true) {
+      console.log("in object ", obj);
+    }
     // // console.log(`{${Object.entries(obj).map(val => {
     // //   console.log(val);
     // //   return `{${val[0]} : ${stringifyJSON(val[1])}}`
@@ -61,16 +70,22 @@ var stringifyJSON = function (obj) {
     let objString = "{";
     for (let [key, value] of Object.entries(obj)) {
 
-      if (typeof value === "undefined" || typeof key === "function") {
-        return "[{}]"
+      if (typeof value === "undefined" || typeof value === "function") {
+        objString += `eraseMePlease`;
       }
 
-      if (curIdx < finalLength) {
-        objString += `"${key}":${stringifyJSON(value)},`
-      } else {
-        objString += `"${key}":${stringifyJSON(value)}`
+      else {
+
+        if (curIdx < finalLength) {
+          objString += `"${key}":${stringifyJSON(value)},`
+        } else {
+          objString += `"${key}":${stringifyJSON(value)}`
+        }
+        curIdx++;
+
       }
-      curIdx++;
+
+
     }
 
     objString += "}";
@@ -89,4 +104,9 @@ var stringifyJSON = function (obj) {
 
 
 // console.log("testing");
-// stringifyJSON();
+stringifyJSON([
+  {
+    'functions': function () { },
+    'undefined': undefined
+  }
+], "debug");
